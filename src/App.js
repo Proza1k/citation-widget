6e0576b6d1fcb08e848c29ebb3559184f253039e
Proza1k <vdovin_yar@mail.ru>
2021-08-02 19:04:01 +0300

    feat: addition new npm component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React                from 'react'
+
+import CitationWidget        from 'components/citation-widget'
+
+class App extends React.Component {
+  render() {
+    return (
+      <CitationWidget />
+    )
+  }
+}
+
+export default App
